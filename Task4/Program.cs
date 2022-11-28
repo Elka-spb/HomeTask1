a74efea54d6c485dc8fce65c300006ee1a65eae2
Elena <el.kalenova@mail.ru>
2022-11-29 00:59:06 +0300

    Task

---
+++ Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿//task 4
+Console.Clear();
+Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a >= b && a >= c) 
+    Console.WriteLine(a);
+if (b >= a && b >= c) 
+    Console.WriteLine(b);
+if (c >= a && c >= b) 
+    Console.WriteLine(c);
